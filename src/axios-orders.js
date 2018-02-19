b57bd1561bbb65ad3593388b4260eaa301e92dc8
Eugenio Valeiras <eugenio.valeiras@gmail.com>
2018-02-19 11:57:55 -0300

    burger-redux-adv

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burger.firebaseio.com/'
+    baseURL: 'https://burger-app-d1780.firebaseio.com/'
 });
 
 export default instance;
